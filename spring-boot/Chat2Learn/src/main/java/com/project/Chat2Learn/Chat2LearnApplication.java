99f3db9e8ea595c2543b85b4b239a449d0fe2124
Sefa <sefa.gokceoglu@bigdatateknoloji.com>
2022-10-16 17:30:43 +0300

    added spring boot project

---
+++ spring-boot/Chat2Learn/src/main/java/com/project/Chat2Learn/Chat2LearnApplication.java
@@ -0,0 +1,13 @@
+package com.project.Chat2Learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat2LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chat2LearnApplication.class, args);
+	}
+
+}
